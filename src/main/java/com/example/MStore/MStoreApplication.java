6b0185ec5c209d2a2f69484a5ae02248407e495e
moss <musa@qualica.com>
2025-12-01 18:21:26 +0200

    fisrt commit

---
+++ src/main/java/com/example/MStore/MStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.MStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MStore/MStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
